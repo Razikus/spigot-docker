9a29c4f2d7ed9a823679e2e9287958451655168f
Adam Raźniewski <adam@razniewski.eu>
2019-03-03 14:19:57 +0000

    arm support

--- autocompiler/entrypoint.sh
+++ autocompiler/entrypoint.sh
@@ -1,7 +1,5 @@
-#!/bin/ash
-
 echo "/usr/bin/java -jar /tools.jar ${@}"
-/usr/bin/java -jar /tools.jar ${@}
+/usr/bin/java -jar -Xmx1024M  /tools.jar ${@}
 
 cp /spigot*.jar /artifacts
 cp /craftbukkit*.jar /artifacts
