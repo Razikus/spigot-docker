ba26f7cb61b7de90562c2055b4eca88542f2660a
Adam Raźniewski <adam@razniewski.eu>
2019-09-27 18:51:57 +0200

    udate

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/ash
+if [ -f "/initialized" ]; then
+	/usr/bin/java -jar /server/spigot.jar ${@}
+else
+	echo "eula=$ACCEPTEULA" > /server/eula.txt
+	mv /spigot.jar /server/
+	touch /initialized
+	/usr/bin/java -jar /server/spigot.jar ${@}
+fi
 
-echo "eula=$ACCEPTEULA" > /eula.txt
-echo "/usr/bin/java -jar /spigot.jar ${@}"
-/usr/bin/java -jar /spigot.jar ${@}
